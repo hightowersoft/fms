9d16ee28e4a89055cad351fa63a7288463d2eb5e
Kerry Moralee <kmoralee1@gmail.com>
2014-10-15 14:14:23 +0100

    Theme and News Controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FootballManagementSystem::Application.config.secret_key_base = '14ea5de2757e10dddd48ecbdc7bec7c65caed285b5793e7b3e08ce5964e2a68bcf887fd072b89676cb363d2e31874e5afac45f94ccb9e8707d1562d8ac1688be'
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+NewsItem.create(title: "News Item 1", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales euismod tristique. Nam sagittis justo sed cursus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis nec justo sit amet elementum. Nunc iaculis feugiat pharetra. Nunc venenatis ante lectus, ac elementum ligula consequat eu. Cras dignissim ante a nisi luctus pellentesque. Proin pulvinar enim massa, at faucibus diam gravida vel. Aliquam lorem dolor, fringilla eu nulla eu, ultricies consequat urna.")
+NewsItem.create(title: "News Item 2", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales euismod tristique. Nam sagittis justo sed cursus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis nec justo sit amet elementum. Nunc iaculis feugiat pharetra. Nunc venenatis ante lectus, ac elementum ligula consequat eu. Cras dignissim ante a nisi luctus pellentesque. Proin pulvinar enim massa, at faucibus diam gravida vel. Aliquam lorem dolor, fringilla eu nulla eu, ultricies consequat urna.")
+NewsItem.create(title: "News Item 3", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales euismod tristique. Nam sagittis justo sed cursus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis nec justo sit amet elementum. Nunc iaculis feugiat pharetra. Nunc venenatis ante lectus, ac elementum ligula consequat eu. Cras dignissim ante a nisi luctus pellentesque. Proin pulvinar enim massa, at faucibus diam gravida vel. Aliquam lorem dolor, fringilla eu nulla eu, ultricies consequat urna.")

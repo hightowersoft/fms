b190637a68754263a9630d3d599f26cf97e7620b
Kerry Moralee <kmoralee1@gmail.com>
2014-10-16 14:43:24 +0100

    Dropdowns in Edit

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -1,3 +1,5 @@
 class Fixture < ActiveRecord::Base
 	has_many :statistics
+	belongs_to :home, class_name: "Team", foreign_key: "home_team"
+	belongs_to :away, class_name: "Team", foreign_key: "away_team"
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
 	validates :short_name, :long_name, presence: true
+	has_many :fixtures
 end
